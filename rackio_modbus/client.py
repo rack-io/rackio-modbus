c07037518ecd5fe1799e6d685de3878b7425b717
Nelson Carrasquel <carrasquel@outlook.com>
2020-11-11 13:49:05 -0400

    Better server implementation

--- rackio_modbus/client.py
+++ rackio_modbus/client.py
@@ -13,10 +13,27 @@ sock.connect(('localhost', 502))
 
 # Returns a message or Application Data Unit (ADU) specific for doing
 # Modbus TCP/IP.
-message = tcp.write_multiple_coils(slave_id=1, starting_address=1, values=[1, 0, 1, 1])
+message = tcp.write_multiple_coils(slave_id=1, starting_address=1, values=[1, 1, 0, 1])
+
 
 # Response depends on Modbus function code. This particular returns the
 # amount of coils written, in this case it is.
 response = tcp.send_message(message, sock)
+
+print(response)
+
+message = tcp.read_coils(slave_id=1, starting_address=1, quantity=4)
+response = tcp.send_message(message, sock)
+
+print(response)
+
+message = tcp.read_discrete_inputs(slave_id=1, starting_address=1, quantity=4)
+
+
+# Response depends on Modbus function code. This particular returns the
+# amount of coils written, in this case it is.
+response = tcp.send_message(message, sock)
+
+print(response)
 
 sock.close()
